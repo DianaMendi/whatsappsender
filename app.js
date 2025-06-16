2fc05036c2d2f66d5376c9fd0eb30a8e1bfb7fee
Diana Mendizabal <artedeamorperu@gmail.com>
2025-06-15 22:48:45 -0500

    sacar el elemento p

--- app.js
+++ app.js
@@ -99,7 +99,7 @@ function handleSignoutClick() {
     if (token !== null) {
         google.accounts.oauth2.revoke(token.access_token);
         gapi.client.setToken('');
-        document.getElementById('content').innerText = '';
+        //document.getElementById('content').innerText = '';
         document.getElementById('authorize_button').innerText = 'Authorize';
         document.getElementById('signout_button').style.visibility = 'hidden';
     }
