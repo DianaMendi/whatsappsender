9ea5dea9e9b9a974286fbebead92eea782baa648
Diana Mendizabal <artedeamorperu@gmail.com>
2025-06-15 21:38:49 -0500

    app.js el gis

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ let gapiInited = false;
 let gisInited = false;
 
 document.getElementById("gapi").addEventListener("load",gapiLoaded());
-document.getElementById("gis").addEventListener("load",gisInited());
+document.getElementById("gis").addEventListener("load",gisLoaded());
 
 document.getElementById('authorize_button').style.visibility = 'hidden';
 document.getElementById('signout_button').style.visibility = 'hidden';
